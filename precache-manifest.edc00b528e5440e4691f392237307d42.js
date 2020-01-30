dd38f2127314313d46fb08e431af609ce5db5c52
GoardiaMC <53571448+GoardiaMC@users.noreply.github.com>
2020-01-30 15:40:04 +0000

    changing to excel project

---
+++ precache-manifest.edc00b528e5440e4691f392237307d42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89df9749125e5f19507102ce56dbe28b",
+    "url": "/ExcelAutomation/index.html"
+  },
+  {
+    "revision": "e4bdb90cb9bbcbbbff60",
+    "url": "/ExcelAutomation/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8eff3b18f613202ce1e7",
+    "url": "/ExcelAutomation/static/js/2.d3d01538.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ExcelAutomation/static/js/2.d3d01538.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4bdb90cb9bbcbbbff60",
+    "url": "/ExcelAutomation/static/js/main.8d939174.chunk.js"
+  },
+  {
+    "revision": "0531b8c2015b8a57467c",
+    "url": "/ExcelAutomation/static/js/runtime-main.885f0cbe.js"
+  }
+]);
