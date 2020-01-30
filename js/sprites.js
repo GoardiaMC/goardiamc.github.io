9485d55b6d27b09d8eb37eb3503397b7d7adcbac
GoardiaMC <53571448+GoardiaMC@users.noreply.github.com>
2020-01-30 17:48:53 +0000

    Replacing

---
+++ js/sprites.js
@@ -0,0 +1,35 @@
+var interval;
+
+/* Image Resources */
+function newImg(src){
+    var image = new Image();
+    image.src = src;
+
+    return image;
+}
+
+function newImgOnload(src){
+    var image = new Image();
+    image.onload = () => {
+        interval = setInterval(draw, tickMs)
+    };
+    image.src = src;
+
+    return image;
+}
+
+img = {
+    '1' : newImg('resources/1.png'),
+    '2' : newImg('resources/2.png'),
+    '3' : newImg('resources/3.png'),
+    '4' : newImg('resources/4.png'),
+    '5' : newImg('resources/5.png'),
+    '6' : newImg('resources/6.png'),
+    '7' : newImg('resources/7.png'),
+    'white' : newImg('resources/white.png'),
+    'bomb' : newImg('resources/bomb.png'),
+    'flagged' : newImg('resources/flagged.png'),
+    'unclicked' : newImgOnload('resources/unclicked.png'),
+    'emoji' : newImg('resources/emoji-happy.png')
+}
+
